3e961002b713447c4ed85f0c18085c9f55ab6156
felipe <manfeltor@live.com>
2023-09-12 21:37:29 -0300

    creadas las 3 clases en la app

--- E3app/models.py
+++ E3app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+
+class Proveedor(models.Model):
+    nombre = models.CharField(max_length=100)
+    rubro = models.CharField(max_length=20)
+    mail = models.EmailField(null= True, blank= True)
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    ciudad = models.CharField(max_length=20)
+    activo = models.BooleanField()
+
+class Venta(models.Model):
+    articulo = models.CharField(max_length=300)
+    monto = models.IntegerField()
+
+    def today(self):
+        return date.today()
+    
+    fecha = models.DateField(default=today)
