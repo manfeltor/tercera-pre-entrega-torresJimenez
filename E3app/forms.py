4b9ac737c7107a9b7de20b5cd16ff9887042e9b6
felipe <manfeltor@live.com>
2023-09-14 22:39:03 -0300

    hechos los modelos de formularios, migradas las nuevas bases (para eso eliminmadas las viejas de forma manual

--- E3app/forms.py
+++ E3app/forms.py
@@ -4,7 +4,7 @@ class FormProveedor(forms.Form):
 
     nombre = forms.CharField(max_length=100)
     rubro = forms.CharField(max_length=20)
-    mail = forms.EmailField(null= True, blank= True)
+    mail = forms.EmailField(empty_value="vacio")
 
 class FormProveedor(forms.Form):
 
